2bcca15b7b68049f67c59ec8cab04fb908209d7b
Soniom04 <122102740+Soniom04@users.noreply.github.com>
2024-06-27 19:14:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mullish", "sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightblue:"#2b84ea",
+        lightblue300: "#4b94ed",
+        lightblue500: "#0b72e7",
+        greenlight: "#61cea6",
+        grayText: "#818597",
+        lightgray: "#e2e2e2",
+        grayblue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+    
+      },
+    },
+  },
+  plugins: [],
+}
